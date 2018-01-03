c366a1e87dcc3bd19caf7a77f1492961b51a15b5
Khalid Mammadov <xmamedov@hotmail.com>
2018-01-03 22:17:37 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hbase_docker
-Hbase cluster on Docker containers
+Hbase cluster on Docker containers.
+See below article about this repo:
+ http://www.khalidmammadov.co.uk/2018/01/03/installing-apache-distributed-hbase-on-docker-containers/
--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
+#Clean
 docker stop hbase_regionserver2 hbase_regionserver_backup hbase_master
 
 docker ps -a| grep hbase| awk '{system("docker rm " $1)}'
 
 
 
-
+#Run
 docker run --name hbase_regionserver_backup -itd --net=pubnet \
       --ip=192.168.1.41 hbase:0.6 \
       /bin/bootstrap_region.sh
